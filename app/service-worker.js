beb016cd71d9e55bbf8995b5ac9998b5a2b2959b
taxman407-gpt <taxman407@gmail.com>
2026-07-27 20:29:38 -0400

    Add MealDaddy app app/service-worker.js

---
+++ app/service-worker.js
@@ -0,0 +1,20 @@
+const CACHE = "mealdaddy-shell-v1";
+const SHELL = ["./", "./index.html", "./app.html", "./styles.css", "./app.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) =>
+    Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET" || new URL(event.request.url).origin !== location.origin) return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
+
